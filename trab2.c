046949ac95f90f7402d12ad278cc26c6d2bf0e48
RafaelJacobb <jrafaeljacob9@gmail.com>
2023-08-06 20:28:50 -0400

    Update trab2.c
    Este é o segundo trabalho pedido em aula

--- trab2.c
+++ trab2.c
@@ -1,46 +1,21 @@
-//Representa a string de entrada, e retorna um ponteiro para um caractere, que será a substring mais longa.
-char *longestNiceSubstring(char *s) {
-    int n = strlen(s); // Obtém o comprimento da string de entrada
-char *resultado = ""; // Inicializa a variável resultado como uma string vazia
+int countStudents(int* students, int studentsSize, int* sandwiches, int sandwichesSize){
+    // Inicialização do array 'count' para contar as preferências dos estudantes por tipo de sanduíche (0 ou 1).
+    int count[2] = {0};
 
-// Loop principal para percorrer a string s
-for (int i = 0; i < n; i++) {
-    int *letrasMinusculas = (int *)calloc(26, sizeof(int));
-    int *letrasMaiusculas = (int *)calloc(26, sizeof(int));
-    
-    // Loop aninhado para percorrer a string a partir da posição i e verifica se maiúscula ou minúscula
-    for (int j = i; j < n; j++) {
-        if (s[j] >= 'a' && s[j] <= 'z')
-            letrasMinusculas[s[j] - 'a'] = 1;
-        else if (s[j] >= 'A' && s[j] <= 'Z')
-            letrasMaiusculas[s[j] - 'A'] = 1;
-        
-        int letrasMinusculasIguais = 1;
-        int letrasMaiusculasIguais = 1;
-        
-         // Loop para comparar os arrays de letras minúsculas e maiúsculas
-        for (int k = 0; k < 26; k++) {
-            if (letrasMinusculas[k] != letrasMaiusculas[k]) {
-                letrasMinusculasIguais = 0;
-                letrasMaiusculasIguais = 0;
-                break;
-            }
-        }
-        
-        // Se as letras minúsculas e maiúsculas são iguais, encontramos uma substring válida e calcula o comprimento da substring atual
-        if (letrasMinusculasIguais && letrasMaiusculasIguais) {
-            int comprimento = j - i + 1;
-
-            // Se o comprimento atual é maior que o comprimento da substring armazenada em resultado e atualiza a substring
-            if (comprimento > strlen(resultado))
-                resultado = strndup(s + i, comprimento);
-        }
+    // Loop para contar as preferências dos estudantes.
+    for(int i = 0; i < studentsSize; i++){
+        count[students[i]]++;  // Incrementa o contador para o tipo de sanduíche que o estudante prefere.
     }
-    
-    // Libera a memória alocada para o array das letras.
-    free(letrasMinusculas);
-    free(letrasMaiusculas);
-}
 
-return resultado;
+    // Loop para distribuir os sanduíches e verificar se todos os estudantes foram atendidos.
+    for(int i = 0; i < sandwichesSize; i++){
+        if(count[sandwiches[i]] == 0){
+            // Se não houver estudantes que queiram esse tipo de sanduíche, retorna quantos estudantes ficarão sem.
+            return sandwichesSize - i;
+        }
+        count[sandwiches[i]]--;  // Decrementa o contador para indicar que um sanduíche desse tipo foi distribuído.
+    }
+
+    // Se todos os estudantes receberam um sanduíche de acordo com suas preferências, retorna 0.
+    return 0;
 }
